b8fb8ed738ed8b746f5326bb57c1b6bbc69dc858
Ricardo Rainha <ricardofrainha@gmail.com>
2019-03-22 19:00:16 -0300

    Convert MainActivity to MVVM
    Signed-off-by: Ricardo Rainha <ricardofrainha@gmail.com>

--- app/src/main/java/com/ricardorainha/mustache/view/MainActivity.java
+++ app/src/main/java/com/ricardorainha/mustache/view/MainActivity.java
@@ -1,6 +1,9 @@
 package com.ricardorainha.mustache.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.Observable;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +13,7 @@ import android.view.MenuItem;
 
 import com.ricardorainha.mustache.R;
 import com.ricardorainha.mustache.authentication.AuthManager;
+import com.ricardorainha.mustache.databinding.ActivityMainBinding;
 import com.ricardorainha.mustache.model.Session;
 import com.ricardorainha.mustache.utils.FirebaseUtils;
 import com.ricardorainha.mustache.utils.SharedPrefUtils;
@@ -17,31 +21,31 @@ import com.ricardorainha.mustache.utils.SharedPrefUtils;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+    private MainActivityViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        configureFacebookSDK();
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        viewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+
+        configureObservables();
+        configureFields();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        if (shouldShowLogin()) {
+        if (AuthManager.getInstance().needToLogin()) {
             Intent loginIntent = new Intent(this, LoginActivity.class);
             startActivity(loginIntent);
             finish();
         }
         else {
-            FirebaseUtils.requestUserInfo(AuthManager.getInstance().getUser().getUid(), user -> {
-                if (user != null) {
-                    Session.getInstance().setUser(user);
-                }
-                if (!SharedPrefUtils.hasCompletedProfile(MainActivity.this)) {
-                    startProfileActivity();
-                }
-            });
+            viewModel.requestUserInfo();
         }
     }
 
@@ -67,8 +71,21 @@ public class MainActivity extends AppCompatActivity {
 //        AppEventsLogger.activateApp(this.getApplication());
 //    }
 
-    private boolean shouldShowLogin() {
-        return AuthManager.getInstance().needToLogin();
+    private void configureObservables() {
+        viewModel.getUserInfoReceived().addOnPropertyChangedCallback(new Observable.OnPropertyChangedCallback() {
+            @Override
+            public void onPropertyChanged(Observable sender, int propertyId) {
+                if (viewModel.getUserInfoReceived().get()) {
+                    if (!SharedPrefUtils.hasCompletedProfile(MainActivity.this)) {
+                        startProfileActivity();
+                    }
+                }
+            }
+        });
+    }
+
+    private void configureFields() {
+
     }
 
     private void startProfileActivity() {
---
+++ app/src/main/java/com/ricardorainha/mustache/view/MainActivityViewModel.java
@@ -0,0 +1,31 @@
+package com.ricardorainha.mustache.view;
+
+import com.ricardorainha.mustache.authentication.AuthManager;
+import com.ricardorainha.mustache.model.Session;
+import com.ricardorainha.mustache.utils.FirebaseUtils;
+
+import androidx.databinding.ObservableField;
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel {
+
+    private ObservableField<Boolean> userInfoReceived = new ObservableField<>();
+
+    public MainActivityViewModel() {
+        userInfoReceived.set(false);
+    }
+
+    public ObservableField<Boolean> getUserInfoReceived() {
+        return userInfoReceived;
+    }
+
+    public void requestUserInfo() {
+        FirebaseUtils.requestUserInfo(AuthManager.getInstance().getUser().getUid(), user -> {
+            if (user != null) {
+                Session.getInstance().setUser(user);
+            }
+
+            userInfoReceived.set(true);
+        });
+    }
+}
